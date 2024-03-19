a33ee7a53d584257d5b91a538f2e3cc4035d3b02
Nakul240 <nakulnanjappa240@gmail.com>
2024-03-19 09:22:34 +0530

    Truck service modified

--- src/main/java/com/ff/logisticsmanangement/service/TruckService.java
+++ src/main/java/com/ff/logisticsmanangement/service/TruckService.java
@@ -1,12 +1,16 @@
 package com.ff.logisticsmanangement.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.ff.logisticsmanangement.dao.CarrierRepository;
 import com.ff.logisticsmanangement.dao.TruckRepository;
 import com.ff.logisticsmanangement.dto.ResponseStructure;
+import com.ff.logisticsmanangement.entity.Carrier;
 import com.ff.logisticsmanangement.entity.Truck;
 import com.ff.logisticsmanangement.util.TruckStatus;
 
@@ -15,17 +19,23 @@ public class TruckService {
 
 	@Autowired
 	private TruckRepository repository;
-	
-	public ResponseEntity<ResponseStructure<Truck>> addTruck(Truck truck) {
-		truck.setStatus(TruckStatus.AVAILABLE);
-		Truck savedTruck = repository.save(truck);
-		
-		ResponseStructure<Truck> structure = new ResponseStructure<Truck>();
-		structure.setStatusCode(HttpStatus.CREATED.value());
-		structure.setMessage("Success");
-		structure.setData(savedTruck);
 
-		return new ResponseEntity<ResponseStructure<Truck>>(structure, HttpStatus.CREATED);
+	private CarrierRepository carrierRepository;
+
+	public ResponseEntity<ResponseStructure<Truck>> addTruck(Truck truck) {
+		Optional<Carrier> opt = carrierRepository.findById(truck.getCarrierId());
+		if (opt != null) {
+			truck.setStatus(TruckStatus.AVAILABLE);
+			Truck savedTruck = repository.save(truck);
+			
+			ResponseStructure<Truck> structure = new ResponseStructure<Truck>();
+			structure.setStatusCode(HttpStatus.CREATED.value());
+			structure.setMessage("Success");
+			structure.setData(savedTruck);
+
+			return new ResponseEntity<ResponseStructure<Truck>>(structure, HttpStatus.CREATED);
+		}
+		return null;
 	}
 
 }
