98da305ea4be3d8a78724bf107257acfe910c4b9
MdShayequeZiya <shayequeziya@outlook.com>
2024-03-19 18:12:34 +0530

    Adding geolocation api

---
+++ src/main/java/com/ff/logisticsmanangement/dto/EstimatesDto.java
@@ -0,0 +1,15 @@
+package com.ff.logisticsmanangement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EstimatesDto {
+	
+	private double distance;
+	private long time;
+
+}
