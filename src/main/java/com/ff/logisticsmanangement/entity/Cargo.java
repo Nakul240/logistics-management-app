058a13b123b4ceee6cf087d10c5b94c65b91bc4b
Anugrahas2001 <anugrahas2001@gmail.com>
2024-03-19 15:12:44 +0530

    Cargo and Unloading enity added

---
+++ src/main/java/com/ff/logisticsmanangement/entity/Cargo.java
@@ -0,0 +1,22 @@
+package com.ff.logisticsmanangement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Data;
+
+@Entity
+@Data
+public class Cargo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "cargo_id")
+	@SequenceGenerator(name = "cargo_id", initialValue = 700, allocationSize = 1, sequenceName = "cargo_sequence")
+	private int cargoId;
+	private String cargoName;
+	private String cargoDescription;
+	private double cargoWeight;
+	private int cargoCount;
+
+}
