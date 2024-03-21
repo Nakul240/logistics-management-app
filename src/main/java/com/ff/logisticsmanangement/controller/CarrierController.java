977c5b758fde1615092b088888b64bf0bd0a6452
GopikaVijayanS <153484878+gopikavijayans001@users.noreply.github.com>
2024-03-21 14:39:29 +0530

    swagger

--- src/main/java/com/ff/logisticsmanangement/controller/CarrierController.java
+++ src/main/java/com/ff/logisticsmanangement/controller/CarrierController.java
@@ -28,7 +28,7 @@ public class CarrierController {
 	@Autowired
 	private CarrierService carrierService;
 
-    //save the carrier details
+	// save the carrier details
 	@Operation(description = "Carrier details will be saved in the database", summary = "To Create Carrier info")
 	@ApiResponses(value = { @ApiResponse(description = "Created", responseCode = "201"),
 			@ApiResponse(content = @Content(), responseCode = "400") })
@@ -37,7 +37,7 @@ public class CarrierController {
 		return carrierService.createCarrier(carrier);
 	}
 
-    //get the carrier details by id
+	// get the carrier details by id
 	@Operation(description = "Carrier details will be retrived from the database", summary = "To get  Carrier info")
 	@ApiResponses(value = { @ApiResponse(description = "OK", responseCode = "200"),
 			@ApiResponse(content = @Content(), responseCode = "400") })
@@ -46,7 +46,7 @@ public class CarrierController {
 		return carrierService.getCarrier(carrierId);
 	}
 
-    //update the carrier details
+	// update the carrier details
 	@Operation(description = "Carrier Details will be updated into the database", summary = "To Update Carrier info")
 	@ApiResponses(value = { @ApiResponse(description = "OK", responseCode = "200"),
 			@ApiResponse(content = @Content(), responseCode = "400") })
@@ -56,7 +56,7 @@ public class CarrierController {
 		return carrierService.updateCarrier(carrierId, carrier);
 	}
 
-    // delete the carrier details
+	// delete the carrier details
 
 	@Operation(description = "Carrier will be deleted from the database", summary = "To Delete Carrier Info")
 	@ApiResponses(value = { @ApiResponse(description = "OK", responseCode = "200"),
@@ -67,7 +67,7 @@ public class CarrierController {
 		return carrierService.deleteCarrier(carrierId);
 	}
 
-    //get all carrier Details
+	// get all carrier Details
 
 	@Operation(description = " All Carrier  details will be retrived from the database", summary = "To get all  Carrier info")
 	@ApiResponses(value = { @ApiResponse(description = "OK", responseCode = "200"),
