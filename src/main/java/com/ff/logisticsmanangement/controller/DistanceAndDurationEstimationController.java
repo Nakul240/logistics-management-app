9207ef76a47c422547f2c1a850789b6f52ec63f2
Nakul240 <nakulnanjappa240@gmail.com>
2024-03-21 15:51:12 +0530

    carrier Authorization

--- src/main/java/com/ff/logisticsmanangement/controller/DistanceAndDurationEstimationController.java
+++ src/main/java/com/ff/logisticsmanangement/controller/DistanceAndDurationEstimationController.java
@@ -2,6 +2,7 @@ package com.ff.logisticsmanangement.controller;
 
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class DistanceAndDurationEstimationController {
 	private DistanceAndDurationEstimationService service;
 	
 	@GetMapping("/estimate")
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
 	public EstimatesDto demo(@RequestParam String loadingAddress, @RequestParam String unloadingAddress) throws ParseException {
 		
 		return service.getTheDistanceAndTime(loadingAddress, unloadingAddress);
