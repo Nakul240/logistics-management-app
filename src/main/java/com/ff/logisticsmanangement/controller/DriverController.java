860843d26bdf8667d7136ab2af01a960b190889c
MdShayequeZiya <shayequeziya@outlook.com>
2024-03-21 15:04:19 +0530

    swagger and authorization done for the driver class

--- src/main/java/com/ff/logisticsmanangement/controller/DriverController.java
+++ src/main/java/com/ff/logisticsmanangement/controller/DriverController.java
@@ -1,6 +1,7 @@
 package com.ff.logisticsmanangement.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,13 @@ import com.ff.logisticsmanangement.dto.ResponseStructure;
 import com.ff.logisticsmanangement.entity.Driver;
 import com.ff.logisticsmanangement.service.DriverService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
-@RequestMapping("/logistics/driver")
+@RequestMapping("/logistics/drivers")
 public class DriverController {
 	
 	private DriverService driverService;
@@ -26,6 +32,9 @@ public class DriverController {
 	}
 	
 	
+	@Operation(description = "To save driver information", summary = "saving driver information" )
+	@ApiResponses(value = {@ApiResponse(description = "Created", responseCode = "201", content = @Content)})
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
 	@PostMapping("/")
 	public ResponseEntity<ResponseStructure<Driver>> saveDriver(@RequestBody DriverDto driverDto){
 		
@@ -33,7 +42,9 @@ public class DriverController {
 		
 	}
 	
-	
+	@Operation(description = "To get a driver through specific driver Id", summary = "get a driver" )
+	@ApiResponses(value = {@ApiResponse(description = "OK", responseCode = "200", content = @Content)})
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
 	@GetMapping("/{driverId}")
 	public ResponseEntity<ResponseStructure<Driver>> getDriverById(@PathVariable int driverId){
 		
@@ -41,6 +52,9 @@ public class DriverController {
 		
 	}
 	
+	@Operation(description = "To update driver info through specific driver Id in the DB", summary = "Update driver info" )
+	@ApiResponses(value = {@ApiResponse(description = "OK", responseCode = "200", content = @Content)})
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'DRIVER')")
 	@PutMapping("/{driverId}")
 	public ResponseEntity<ResponseStructure<String>> updateDriver(@PathVariable int driverId, @RequestBody DriverDto driverDto){
 		
@@ -48,6 +62,9 @@ public class DriverController {
 		
 	}
 	
+	@Operation(description = "To delete a driver through specific driver Id", summary = "delete a driver" )
+	@ApiResponses(value = {@ApiResponse(description = "OK", responseCode = "200", content = @Content)})
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
 	@DeleteMapping("/{driverId}")
 	public ResponseEntity<ResponseStructure<String>> deleteDriver(@PathVariable int driverId){
 		return null;
